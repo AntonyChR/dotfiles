0a73269efca6699888ed2dbc71cf6d544f4651c4
AntonyChR <antonychr99@gmail.com>
2024-06-28 10:15:19 -0500

    update fzf config

--- bash/config.sh
+++ bash/config.sh
@@ -46,6 +46,8 @@ alias glog='git log --oneline'
 alias res='source ~/.bashrc'
 
 alias fe='dolphin'
+
+export FZF_DEFAULT_OPTS='--color=bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672'
 alias f='fzf --bind "enter:become($EDITOR {})" --height 90% --layout reverse --border --preview "cat {}"'
 alias cl='clear'
 alias vi='vim'

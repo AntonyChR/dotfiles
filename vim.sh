ef8ffee2f21987872d14738cc2bf1673763dcea1
AntonyChR <antonychr99@gmail.com>
2024-07-31 19:06:39 -0500

    update

--- vim.sh
+++ vim.sh
@@ -3,5 +3,6 @@ vim_dir="$(pwd)/vim"
 for file in $vim_dir/*; do
     echo "source $file" >> ~/.vimrc
 done
+ln -s "$(pwd)/vim/coc-settings.json" ~/.vim/coc-settings.json
 
 echo "Vim files linked to .vimrc"
